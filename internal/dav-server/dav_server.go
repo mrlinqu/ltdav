e2fbb2bec0ef7d93dc290b45d1aeb556d32f2fa0
mr.linqu <mr.linqu@gmail.com>
2025-05-18 14:18:39 +0300

    [] tls debuging;;

--- internal/dav-server/dav_server.go
+++ internal/dav-server/dav_server.go
@@ -2,14 +2,14 @@ package dav_server
 
 import (
 	"context"
+	"crypto/tls"
+	"fmt"
 	"log"
 	"net/http"
 
-	http_auth "github.com/mrlinqu/ltdav/internal/http-auth"
-	secret_provider "github.com/mrlinqu/ltdav/internal/http-auth/secret-provider"
+	x509_keypair_reloader "github.com/mrlinqu/ltdav/internal/x509-keypair-reloader"
 	"github.com/pkg/errors"
 	zlog "github.com/rs/zerolog/log"
-	"golang.org/x/net/webdav"
 )
 
 type DavServer struct {
@@ -46,6 +46,39 @@ func (s *DavServer) WithAuth(passwdFilePath string, realm string) *DavServer {
 	return s
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from secure server!\n")
+}
+func setupRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloHandler)
+	return mux
+}
+func configureTLS(certFile, keyFile string) *tls.Config {
+	return &tls.Config{
+		GetCertificate: getCertificateFunc(certFile, keyFile),
+		//Certificates: []tls.Certificate{cert},
+		//MinVersion:   tls.VersionTLS12,
+		// CipherSuites: []uint16{
+		// 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		// },
+		//PreferServerCipherSuites: true,
+	}
+}
+func getCertificateFunc(certFile, keyFile string) func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	return func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+		log.Println("get cert")
+
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed to load certificate: %v", err)
+		}
+
+		return &cert, nil
+	}
+
+}
+
 func (s *DavServer) ListenAndServe(ctx context.Context) error {
 	zlog.Debug().
 		Str("listenAddr", s.listenAddr).
@@ -56,42 +89,69 @@ func (s *DavServer) ListenAndServe(ctx context.Context) error {
 		Str("realm", s.realm).
 		Msg("starting dav server")
 
-	s.srv = &http.Server{
-		Addr: s.listenAddr,
-		Handler: &webdav.Handler{
-			FileSystem: webdav.Dir(s.workingDir),
-			LockSystem: webdav.NewMemLS(),
-			Logger:     s.logger,
-		},
-		ErrorLog: log.New(zlog.Logger, "", 0),
-		//ErrorLog: logger.New(&fwdToZapWriter{logger}, "", 0),
+	handler := setupRouter()
+	tlsCfg := configureTLS("cert.pem", "key.pem")
+
+	srv := &http.Server{
+		Addr:      "0.0.0.0:8443",
+		Handler:   handler,
+		TLSConfig: tlsCfg,
+		//TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+		//ReadTimeout:  5 * time.Second,
+		//WriteTimeout: 10 * time.Second,
+		//IdleTimeout:  120 * time.Second,
 	}
 
-	if s.passwdFilePath != "" {
-		secretProvider, err := secret_provider.NewHtpasswordProvider(s.passwdFilePath)
-		if err != nil {
-			return errors.Wrap(err, "create secret_provider")
-		}
+	s.srv = srv
+	return s.srv.ListenAndServeTLS("", "")
 
-		s.srv.Handler = http_auth.NewBasicAuthInterceptor(s.srv.Handler, secretProvider, s.realm)
+	// s.srv = &http.Server{
+	// 	Addr: s.listenAddr,
+	// 	Handler: &webdav.Handler{
+	// 		FileSystem: webdav.Dir(s.workingDir),
+	// 		LockSystem: webdav.NewMemLS(),
+	// 		Logger:     s.logger,
+	// 	},
+	// 	ErrorLog: log.New(zlog.Logger, "", 0),
+	// 	//ErrorLog: logger.New(&fwdToZapWriter{logger}, "", 0),
+	// }
+
+	// if s.passwdFilePath != "" {
+	// 	secretProvider, err := secret_provider.NewHtpasswordProvider(s.passwdFilePath)
+	// 	if err != nil {
+	// 		return errors.Wrap(err, "create secret_provider")
+	// 	}
+
+	// 	s.srv.Handler = http_auth.NewBasicAuthInterceptor(s.srv.Handler, secretProvider, s.realm)
+	// }
+
+	// tlsConfig, err := s.initTLS(ctx)
+	// if err != nil {
+	// 	return errors.Wrap(err, "initTLS")
+	// }
+
+	// if tlsConfig != nil {
+	// 	s.srv.TLSConfig = tlsConfig
+	// 	return s.srv.ListenAndServeTLS("", "")
+	// }
+
+	// return s.srv.ListenAndServe()
+}
+
+func (s *DavServer) initTLS(ctx context.Context) (*tls.Config, error) {
+	if s.certPath == "" || s.keyPath == "" {
+		return nil, nil
 	}
 
-	if s.certPath != "" || s.keyPath != "" {
-		// keyReloader, err := x509_keypair_reloader.New(ctx, s.certPath, s.keyPath)
-		// if err != nil {
-		// 	return errors.Wrap(err, "create x509_keypair_reloader")
-		// }
-
-		// s.srv.TLSConfig = &tls.Config{
-		// 	//MinVersion:               tls.VersionTLS13,
-		// 	GetCertificate: keyReloader.GetCertificateFunc(),
-		// }
-
-		//return s.srv.ListenAndServeTLS("", "")
-		return s.srv.ListenAndServeTLS(s.certPath, s.keyPath)
+	keyReloader, err := x509_keypair_reloader.New(ctx, s.certPath, s.keyPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "create x509_keypair_reloader")
 	}
 
-	return s.srv.ListenAndServe()
+	return &tls.Config{
+		//MinVersion:               tls.VersionTLS13,
+		GetCertificate: keyReloader.GetCertificateFunc(),
+	}, nil
 }
 
 func (s *DavServer) Shutdown(ctx context.Context) error {
